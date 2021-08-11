a35272d0ab3d91663d2c2e3aad2727fddd75afc8
Ellery Vernon <tmpks3@gmail.com>
2021-08-12 00:44:54 +0900

    20210812

---
+++ graphql/db.js
@@ -0,0 +1,27 @@
+ const movies = [
+    {
+      id: 0,
+      name: "Star Wars - The new one",
+      score: 1
+    },
+    {
+      id: 1,
+      name: "Avengers - The new one",
+      score: 8
+    },
+    {
+      id: 2,
+      name: "The Godfather I",
+      score: 99
+    },
+    {
+      id: 3,
+      name: "Logan",
+      score: 2
+    }
+  ];
+
+export const getById = (id) => {
+  const filteredPeople = people.filter((person) => id === person.id);
+  return filteredPeople[0];
+};
