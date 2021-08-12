1de50e1bc16e25a9466ed69d3732843cffc6069a
Ellery Vernon <tmpks3@gmail.com>
2021-08-12 22:45:16 +0900

    20210812

--- graphql/db.js
+++ graphql/db.js
@@ -1,27 +1,51 @@
- const movies = [
-    {
-      id: 0,
-      name: "Star Wars - The new one",
-      score: 1
-    },
-    {
-      id: 1,
-      name: "Avengers - The new one",
-      score: 8
-    },
-    {
-      id: 2,
-      name: "The Godfather I",
-      score: 99
-    },
-    {
-      id: 3,
-      name: "Logan",
-      score: 2
-    }
-  ];
+import axios from "axios";
+const BASE_URL = "http://yts.mx/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_detail.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getById = (id) => {
-  const filteredPeople = people.filter((person) => id === person.id);
-  return filteredPeople[0];
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios.get(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+    headers: {
+      Accep: "application/json",
+      "Content-Type": "application/json",
+      "X-Forwarded-Proto": "https",
+      Origin: "*",
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios.get(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios.get(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
 };
